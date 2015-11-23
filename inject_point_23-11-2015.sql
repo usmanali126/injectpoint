c1a3d9e03af384bc4c49a0c4e6945c5e8714ede3
usmanali126 <usmanali126@hotmail.com>
2015-11-23 12:00:37 +0500

    Final commit

--- inject_point_23-11-2015.sql
+++ inject_point_23-11-2015.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2015 at 02:30 PM
+-- Generation Time: Nov 23, 2015 at 07:52 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `area` (
   `name` varchar(30) NOT NULL,
   `number` int(11) NOT NULL,
   `hw_name` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `area`
@@ -45,7 +45,8 @@ INSERT INTO `area` (`id`, `concile_id`, `name`, `number`, `hw_name`) VALUES
 (4, 4, 'Paharang 4', 4, 'Kausar'),
 (5, 4, 'Salarwala KLD ', 5, 'Razia'),
 (6, 4, 'Salarwala Qamar', 6, 'Rabina'),
-(7, 2, 'Paropian', 1, 'Bushra');
+(7, 2, 'Paropian', 1, 'Bushra'),
+(8, 6, 'sahianwala', 1, 'rasheed');
 
 -- --------------------------------------------------------
 
@@ -76,21 +77,30 @@ CREATE TABLE IF NOT EXISTS `information` (
   `6_inj` varchar(11) NOT NULL,
   `opv1` varchar(15) NOT NULL,
   `opv2` varchar(15) NOT NULL,
+  `remarks` varchar(255) NOT NULL,
+  `mremarks` varchar(255) NOT NULL,
   `create_date` varchar(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `information`
 --
 
-INSERT INTO `information` (`id`, `concile_id`, `area_id`, `m_card`, `m_name`, `tt1`, `tt2`, `edd`, `card_number`, `name`, `father_name`, `address`, `cell_number`, `dob`, `1_inj`, `2_inj`, `3_inj`, `4_inj`, `5_inj`, `6_inj`, `opv1`, `opv2`, `create_date`) VALUES
-(1, 4, 1, 0, '', '', '', '', '123456', 'Ali', 'Haider', 'Paharang near Jamia Masjid', '03324153317', '01-10-2015', '27-10-2015', '', '', '', '', '', '', '', ''),
-(2, 4, 4, 0, '', '', '', '', '123457', 'Sajaad', 'Rafique', 'Pahrang Near Gulzar e madina Masjid', '03454153317', '08-09-2015', '27-10-2015', '', '', '', '', '', '', '', ''),
-(3, 4, 6, 0, '', '', '', '', '123458', 'Faisal', 'M. Ali', 'Street # 4 Qamar Colony Salarwala', '03454153345', '05-08-2015', '10-08-2015', '18-09-2015', '', '', '', '', '', '', ''),
-(4, 4, 5, 0, '', '', '', '', '123459', 'Khalid', 'Naseem', 'khalid colony Salarwala', '03324153317', '10-07-2015', '20-07-2015', '22-08-2015', '06-09-2015', '', '', '', '', '', ''),
-(5, 4, 4, 0, '', '', '', '', '123460', 'Saleem', 'Jameel', 'Paharang near Jamia Masjid', '03454153317', '15-09-2015', '', '', '', '', '', '', '', '', ''),
-(6, 4, 4, 5233245, 'test', '', '', '06-11-2015', '123461', 'test', 'test', 'Paharang near Jamia Masjid', '03454153317', '08-09-2015', '', '', '', '', '', '', '', '', ''),
-(7, 2, 1, 0, 'Rashida', '', '', '', '123462', 'Ali', 'Jameel', 'main bazar parpopian', '264656565465', '08-07-2015', '', '', '', '', '', '', '', '', '05-11-2015');
+INSERT INTO `information` (`id`, `concile_id`, `area_id`, `m_card`, `m_name`, `tt1`, `tt2`, `edd`, `card_number`, `name`, `father_name`, `address`, `cell_number`, `dob`, `1_inj`, `2_inj`, `3_inj`, `4_inj`, `5_inj`, `6_inj`, `opv1`, `opv2`, `remarks`, `mremarks`, `create_date`) VALUES
+(1, 4, 1, 0, '', '', '', '', '123456', 'Ali', 'Haider', 'Paharang near Jamia Masjid', '03324153317', '01-10-2015', '27-10-2015', '', '', '', '', '', '', '', '', '', ''),
+(2, 4, 4, 0, '', '', '', '', '123457', 'Sajaad', 'Rafique', 'Pahrang Near Gulzar e madina Masjid', '03454153317', '18-08-2015', '01-09-2015', '11-11-2015', '30-11-2015', '', '', '', '', '', '', '', ''),
+(3, 4, 6, 0, '', '', '', '', '123458', 'Faisal', 'M. Ali', 'Street # 4 Qamar Colony Salarwala', '03454153345', '05-08-2015', '10-08-2015', '18-09-2015', '', '', '', '', '', '', '', '', ''),
+(4, 4, 5, 0, '', '', '', '', '123459', 'Khalid', 'Naseem', 'khalid colony Salarwala', '03324153317', '10-07-2015', '20-07-2015', '22-08-2015', '06-09-2015', '', '', '', '20-07-2015', '22-08-2015', '', '', ''),
+(5, 4, 4, 0, '', '', '', '', '123460', 'Saleem', 'Jameel', 'Paharang near Jamia Masjid', '03454153317', '15-09-2015', '', '', '', '', '', '', '', '', '', '', ''),
+(6, 4, 4, 5233245, 'Rabia', '', '', '06-11-2015', '123461', 'Noman', 'Haider', 'Paharang near Jamia Masjid', '03454153317', '08-09-2015', '', '', '', '', '', '', '', '', '', '', ''),
+(7, 2, 1, 0, 'Rashida', '', '', '', '123462', 'Ali', 'Jameel', 'main bazar parpopian', '264656565465', '08-07-2015', '19-08-2015', '16-09-2015', '16-10-2015', '14-11-2015', '', '', '10-08-2015', '', 'child moved', '', '05-11-2015'),
+(8, 4, 4, 5233246, 'Salmaah', '16-11-2015', '30-11-2015', '22-06-2016', '', '', 'Saleem', 'near jamia masjid', '03142555245', '', '', '', '', '', '', '', '', '', '', 'mother target complete', '09-11-2015'),
+(9, 4, 1, 0, 'Farzana', '', '', '', '10', 'zaigham', 'Shafiq Ahmad', '126 rb', '00000000', '02-11-2015', '', '', '', '', '', '', '', '', '', '', '09-11-2015'),
+(10, 2, 1, 0, 'Salmaah', '', '', '', '11', 'Salman', 'Saleem', 'paropian main bazar', '0000000000', '10-04-2014', '10-05-2014', '26-06-2014', '29-07-2014', '27-10-2014', '04-02-2015', '22-10-2015', '10-05-2014', '27-10-2014', 'target complete', '', '14-11-2015'),
+(11, 2, 1, 12345, 'Salmaah', '', '', '07-11-2015', '15', 'saleem', 'sfs', 'paropian main bazar', '23234532452', '25-11-2015', '', '', '', '', '', '', '', '', '', '', '19-11-2015'),
+(12, 4, 6, 0, 'najmah', '', '', '', '12', 'noman', 'naeem', 'Qamar Colony', '23234532452', '19-11-2015', '', '', '', '', '', '', '', '', '', '', '19-11-2015'),
+(13, 2, 1, 123, 'sdf', '19-11-2015', '', '23-11-2015', '345', 'sfvcv', 'sdf', 'sdfsd', '12313', '24-11-2015', '', '', '', '', '', '', '', '', '', '', '23-11-2015'),
+(14, 2, 1, 0, 'sdfs', '', '', '', '1234', 'tet', 'sdf', 'sdf', '1232', '23-11-2015', '18-11-2015', '', '', '', '', '', '', '', '', '', '23-11-2015');
 
 -- --------------------------------------------------------
 
@@ -103,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `un_concile` (
   `name` varchar(30) NOT NULL,
   `number` int(11) NOT NULL,
   `tehsil` varchar(30) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `un_concile`
@@ -113,7 +123,8 @@ INSERT INTO `un_concile` (`id`, `name`, `number`, `tehsil`) VALUES
 (1, 'BehlorPur', 4, 'chak Jhumra'),
 (2, 'chak jhumra', 13, 'chak jhumra'),
 (3, 'Sahian Wala', 6, 'chak jhumra'),
-(4, 'Paropian', 2, 'chak jhumra');
+(4, 'Paropian', 2, 'chak jhumra'),
+(5, 'test', 0, 'test');
 
 -- --------------------------------------------------------
 
@@ -173,17 +184,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `area`
 --
 ALTER TABLE `area`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `information`
 --
 ALTER TABLE `information`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `un_concile`
 --
 ALTER TABLE `un_concile`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `user`
 --
